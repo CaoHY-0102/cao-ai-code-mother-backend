80bacefb757923a8ce2613a921baf90dec461676
caochenxu <1962662569@qq.com>
2025-08-03 20:35:13 +0800

    第二期补充： 添加用户相关功能- 新增用户实体类、控制器、Mapper、服务接口及其实现类 - 实现用户注册、登录、登出、获取用户信息等功能 - 添加用户相关页面的路由和处理逻辑 - 采用MyBatis Flex作为ORM框架，简化数据库操作 - 使用Lombok注解减少样板代码

---
+++ src/main/java/com/cao/caoaicodemother/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.cao.caoaicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.cao.caoaicodemother.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author 小曹同学
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
